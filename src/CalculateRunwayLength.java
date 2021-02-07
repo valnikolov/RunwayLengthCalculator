d41aa03cbf21e2889c7d5a94b60e37d11bc72b4d
Valeri Nikolov <nikolov.val@protonmail.com>
2021-02-07 15:43:33 +0200

    initial commit

---
+++ src/CalculateRunwayLength.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class CalculateRunwayLength {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter airplane velocity in m/s: ");
+        double v = scanner.nextDouble();
+
+        System.out.print("Enter airplane acceleration in m/s^2: ");
+        double a = scanner.nextDouble();
+
+        double minimumRunwayLength = (v * v) / (2 * a);
+
+        System.out.printf("Minimum runway length needed for plane take off: %.2f%n",
+                minimumRunwayLength);
+
+    }
+
+}
